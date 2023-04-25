15763065edcbd9abd5c9a7f659fb3d0b2386a117
Kyle <48182171+Kyle-Burris@users.noreply.github.com>
2023-04-24 20:48:17 -0700

    Driver code for Trips

--- main.cpp
+++ main.cpp
@@ -1,20 +1,36 @@
 #include "mainwindow.h"
-//#include "stadiummanager.h"
+#include "stadiummanager.h"
 
 #include <QApplication>
+
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    StadiumManager sm;
+
+    Trip trip = sm.shortestPath("Marlins Park", "Kauffman Stadium");
+//    Trip trip = sm.DFS("Marlins Park");
+//    Trip trip = sm.BFS("Marlins Park");
+
+    for (auto& stadium : trip.path)
+    {
+        qDebug() << stadium->getStadiumName() << "   "
+                 << stadium->getLocation();
+
+        for (auto& souvenir : stadium->getSouvenirs())
+        {
+            qDebug() << souvenir.name << souvenir.price;
+        }
+
+        qDebug () << "\n";
+    }
+
+    qDebug() << "Distance Traveled: " << trip.distanceTraveled;
+
+
     w.show();
-
-//    StadiumManager sm;
-
-//    sm.printEntrys();
-//    sm.printSouvenirs();
-//    sm.printGraph();
-
-
     return a.exec();
 }
