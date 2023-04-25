77f10d921d7a79e3fff4813457bdae159840e68d
Kyle <48182171+Kyle-Burris@users.noreply.github.com>
2023-04-24 22:07:35 -0700

    MST driver code added

--- main.cpp
+++ main.cpp
@@ -10,25 +10,35 @@ int main(int argc, char *argv[])
     MainWindow w;
 
     StadiumManager sm;
+    
+    MinTree tree = sm.MST("Marlins Park");
 
-    Trip trip = sm.shortestPath("Marlins Park", "Kauffman Stadium");
-//    Trip trip = sm.DFS("Marlins Park");
-//    Trip trip = sm.BFS("Marlins Park");
-
-    for (auto& stadium : trip.path)
+    for (auto& edge : tree.edges)
     {
-        qDebug() << stadium->getStadiumName() << "   "
-                 << stadium->getLocation();
-
-        for (auto& souvenir : stadium->getSouvenirs())
-        {
-            qDebug() << souvenir.name << souvenir.price;
-        }
-
-        qDebug () << "\n";
+        qDebug() << edge.orig << "  ->  "
+                 << edge.dest;
     }
 
-    qDebug() << "Distance Traveled: " << trip.distanceTraveled;
+    qDebug() << "Total Distance:  " << tree.totalDistance;
+
+//     Trip trip = sm.shortestPath("Marlins Park", "Kauffman Stadium");
+// //    Trip trip = sm.DFS("Marlins Park");
+// //    Trip trip = sm.BFS("Marlins Park");
+
+//     for (auto& stadium : trip.path)
+//     {
+//         qDebug() << stadium->getStadiumName() << "   "
+//                  << stadium->getLocation();
+
+//         for (auto& souvenir : stadium->getSouvenirs())
+//         {
+//             qDebug() << souvenir.name << souvenir.price;
+//         }
+
+//         qDebug () << "\n";
+//     }
+
+//     qDebug() << "Distance Traveled: " << trip.distanceTraveled;
 
 
     w.show();
